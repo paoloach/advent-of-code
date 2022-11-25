7c7659ee87720fd2bc25692db3c8f4cca7124180
Paolo Achdjian <pachdjian@gmail.com>
2022-11-25 20:25:12 +0100

    my kotlin version

--- 2021/03/main.kt
+++ 2021/03/main.kt
@@ -1,13 +1,33 @@
 import java.io.File
 
-data class Pos(var horiz:Int, var depth:Int)
+data class OnesZeros(val ones: List<String>, val zeros: List<String>){
+    fun getMostCommon()  = if (ones.size >= zeros.size)
+                              ones
+                            else
+                                zeros
+    fun getLeastCommon()  = if (ones.size < zeros.size)
+                                ones
+                            else
+                                zeros
+}
+
+/*
+fun List<String>.onesZeros(bitIndex: Int):OnesZeros{
+    val ones = filter { it[bitIndex] == '1' }.count()
+    val zeros = size - ones
+    return OnesZeros(ones, zeros)
+}
+*/
+
+fun List<String>.splitByBit(bitIndex:Int) = OnesZeros(filter { it[bitIndex] == '1' }, filter { it[bitIndex] == '0' } )
+
 
 fun main(args: Array<String>) {
-    var pos = Pos(0,0)
     val fileName="input.txt"
+    val data = File(fileName).readLines()
+    val bitsCount = data[0].length
     var bitsCounter = listOf<Int>()
-    File(fileName)
-        .forEachLine {
+    data.forEach {
             var lineBitCounter = mutableListOf<Int>()
             it.forEach { bit->
                 if (bit=='1')
@@ -37,5 +57,27 @@ fun main(args: Array<String>) {
     }.toInt(2)
 
     println(gamma*epsilon)
+
+    val splitted = data.splitByBit(0)
+    var oxigen =splitted.getMostCommon()
+    var co2 = splitted.getLeastCommon();
+
+    for (bitIndex in 1 .. bitsCount){
+        if (oxigen.size > 1){
+            val oxigenSplit = oxigen.splitByBit(bitIndex)
+            oxigen = oxigenSplit.getMostCommon()
+        }
+        if (co2.size > 1){
+            val co2Splitted = co2.splitByBit(bitIndex)
+            co2 = co2Splitted.getLeastCommon()
+        }
+    }
+
+    var oxigenGenerator = oxigen[0].toInt(2)
+    var co2Generator = co2[0].toInt(2)
+
+    println(oxigenGenerator*co2Generator)
+
+
 
 }
