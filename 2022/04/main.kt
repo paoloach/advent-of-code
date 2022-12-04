a207f1a8811fdccd11665e823b9fdde1ad2559d7
Paolo Achdjian <pachdjian@gmail.com>
2022-12-04 06:39:45 +0100

    Day 4

---
+++ 2022/04/main.kt
@@ -0,0 +1,67 @@
+import java.io.File
+
+
+
+fun main(args: Array<String>) {
+    step1()
+    step2()
+}
+
+data class Range(val min:Int, val max: Int)
+
+data class ElvesPair(val first:Range, val second:Range){
+    fun contained() = (first.min <=  second.min && second.max <= first.max)  || (second.min <=  first.min && first.max <= second.max)
+    fun overlap() = (first.min <=  second.min && second.min <= first.max) ||
+                    (first.min <=  second.max && second.max <= first.max) ||
+                    (second.min <=  first.min && first.min <= second.max)  ||
+                    (second.min <=  first.max && first.max <= second.max)
+
+}
+
+fun convertToRange(str: String):Range{
+    val values = str.split("-")
+    return Range(values[0].toInt(), values[1].toInt())
+}
+
+fun step1(){
+
+    val fileName="input.txt"
+    val total = File(fileName)
+        .readLines()
+        .map{it.split(",")}
+        .map{ElvesPair(convertToRange(it[0]), convertToRange(it[1]))}
+        .filter {
+            it.contained()
+        }
+        .size
+
+    println("Total completed overlapped pairs: $total")
+
+
+}
+
+fun step2(){
+
+
+    val fileName="input.txt"
+    val total = File(fileName)
+        .readLines()
+        .map{it.split(",")}
+        .map{ElvesPair(convertToRange(it[0]), convertToRange(it[1]))}
+        .filter {
+            it.overlap()
+        }
+        .size
+
+    println("Total overlapped pairs: $total")
+
+}
+
+fun convertPriority(it: Char):Int {
+    if (it.isLowerCase()){
+        return it.code-'a'.code+1
+    } else {
+        return it.code-'A'.code+27
+    }
+}
+
